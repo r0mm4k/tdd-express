301b8211e7d4c48a71de5510edaa291cbcda7ed5
r0mm4k <r0mmm4k@gmail.com>
2020-10-23 19:48:34 +0300

    - add simple validation

--- src/user/user-router.js
+++ src/user/user-router.js
@@ -4,8 +4,18 @@ const UserService = require("./user-service");
 const router = express.Router();
 
 router.post("/api/1.0/users", async (req, res) => {
-  await UserService.save(req.body);
-  return res.send({ message: "User created" });
+  const { username } = req.body;
+
+  if (username) {
+    await UserService.save(req.body);
+    return res.send({ message: "User created" });
+  } else {
+    return res.status(400).send({
+      errors: {
+        username: "Username is required",
+      },
+    });
+  }
 });
 
 module.exports = router;
